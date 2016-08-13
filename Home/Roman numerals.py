c35e6223f3cdf883c81bcc13fa4ad8c5047d9de4
rgstr123 <rgstr@inbox.lv>
2016-08-14 00:15:47 +0300

    Римские числа

---
+++ Home/Roman numerals.py
@@ -0,0 +1,31 @@
+# coding: utf8
+
+slovarj = ((1000, "M"),
+           (900, "CM"),
+           (500, "D"),
+           (400, "CD"),
+           (100, "C"),
+           (90, "XC"),
+           (50, "L"),
+           (40, "XL"),
+           (10, "X"),
+           (9, "IX"),
+           (5, "V"),
+           (4, "IV"),
+           (1, "I"))
+
+
+def checkio(data):
+    roman = ""
+    for num in slovarj:
+        while data >= num[0]:
+            roman += num[1]
+            data -= num[0]
+            print(roman)
+    return roman
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
