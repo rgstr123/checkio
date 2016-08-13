838353ea6cfdb90b1a618dcfdde6472734dc86e6
rgstr123 <rgstr@inbox.lv>
2016-08-14 01:32:55 +0300

    Класс "Friends"

---
+++ Elementary/Friends.py
@@ -0,0 +1,53 @@
+# coding: uft8
+
+
+class Friends:
+    def __init__(self, connections):
+
+        self.connections = connections
+        self.connections = list(self.connections) # Perevozhu v list, 4to bi leg4e bilo udaljat' dobavljat' elementi
+
+    def add(self, connection):
+        if connection in self.connections:
+            return False
+        else:
+            self.connections.append(connection)
+            return True
+
+
+    def remove(self, connection):
+        if connection in self.connections:
+            self.connections.remove(connection)
+            return True
+        else:
+            return False
+
+    def names(self):
+        all_names = set()
+        for x, y in self.connections:
+            all_names.add(x)
+            all_names.add(y)
+        return all_names
+
+    def connected(self, name):
+        connection_list = set()
+        for x, y in self.connections:
+            if x == name:
+                connection_list.add(y)
+            if y == name:
+                connection_list.add(x)
+        return connection_list
+
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    letter_friends = Friends(({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"}))
+    digit_friends = Friends([{"1", "2"}, {"3", "1"}])
+    assert letter_friends.add({"c", "d"}) is True, "Add"
+    assert letter_friends.add({"c", "d"}) is False, "Add again"
+    assert letter_friends.remove({"c", "d"}) is True, "Remove"
+    assert digit_friends.remove({"c", "d"}) is False, "Remove non exists"
+    assert letter_friends.names() == {"a", "b", "c"}, "Names"
+    assert letter_friends.connected("d") == set(), "Non connected name"
+    assert letter_friends.connected("a") == {"b", "c"}, "Connected name"
