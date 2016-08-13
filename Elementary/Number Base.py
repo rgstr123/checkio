58449d65ffb208b3f3f372b74f8e881af36ac3f1
rgstr123 <rgstr@inbox.lv>
2016-08-14 01:23:16 +0300

    Конвертация числа в десятичную систему счисления

---
+++ Elementary/Number Base.py
@@ -0,0 +1,17 @@
+# coding: utf8
+
+
+def checkio(str_number, radix):
+    try:
+        a = int(str_number, radix)
+    except ValueError:
+        a = -1
+    return a
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("101", 2) == 5, "Bin"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("Z", 36) == 35, "Z base"
+    assert checkio("AB", 10) == -1, "B > A > 10"
