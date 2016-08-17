49aefdc6ee1a8907bd17fc064683fe0061216899
rgstr123 <rgstr@inbox.lv>
2016-08-18 01:23:01 +0300

    Проверить скобки на закрытие   ("{}" "()" или "[]")

--- Electronic-Station/Brackets.py
+++ Electronic-Station/Brackets.py
@@ -1,32 +1,56 @@
 # coding:utf8
-from collections import defaultdict
 
 def checkio(expression):
-    for_check = ["(", ")", "{", "}", "[", "]"]
-    bracket_dic = defaultdict(list)     # создаём словарь в котором значения листы
-    express = list(expression)
-    print("express", express)
-    # res1 = False
-    # res2 = False
-    # res3 = False
-    # if express.count(for_check[0]) == express.count(for_check[1]): res1 = True
-    # elif express.count(for_check[2]) == express.count(for_check[3]): res2 = True
-    # elif express.count(for_check[4]) == express.count(for_check[5]): res3 = True
-    # else: pass
-    #
-    # if not res1 == res2 == res3: return False
+    print(expression)
 
-    for item in for_check:
-        print(bracket_dic)
-        for count_item in expression:
-            if item in express:
-                bracket_dic[item].append(express.index(item))
-                express[express.index(item)] = "."
+    brackets = {'(': ')', '[': ']', '{': '}'}  # Основной словарь
+    valid = [item for items in brackets.items() for item in items] # Список допустимых скобок
+
+    # Перевернутый словарь нужен для доступа к ключам основного словаря, как к значениям (для описания ошибки)
+    reverse = dict((value, key) for key, value in brackets.items())
+
+    lastopened = []
+    status = True
 
 
-    #TODO Осталось сделать: выбрать начальные к конечные скобки и проверить внутри них
+    def check(char):
+        # Если открывающаяся скобка, то добавляем её в список
+        if char in brackets.keys():
+            lastopened.append(char)
 
-    return
+        # Если закрывающаяся скобка
+        else:
+            # Убедимся, что была открывающаяся
+            if len(lastopened) < 1:
+                print('Закрывающая кобка ' + char + ' была до открывающей ' + reverse[char])
+                nonlocal status
+                status = False
+            else:
+                # Убедимся, что закрывающая скобка и открывающая совпадают
+                if char != brackets[lastopened[-1]]:
+                    status = False
+                    print('Должна была быть такая ' + brackets[lastopened[-1]] + ' закрывающая кобка, но такая ' + char + ' встретилась раньше')
+
+                # Если пара скобок была правильно закрыта, то удаляем из списка последнюю открывающую
+                else:
+                    print("Убираю ", lastopened[-1])
+                    lastopened.pop()
+
+
+    for char in range(len(expression)):
+        if expression[char] in valid:
+            check(expression[char])
+
+    if len(lastopened) > 0:
+        status = False
+        print("Не было закрытия скобки " + brackets[lastopened[-1]])
+    else:
+        if status:
+            print("Все скобки зактрыты правильно")
+    print(status)
+    print("-"*50)
+    return status
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
@@ -36,3 +60,4 @@ if __name__ == '__main__':
     assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
     assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
     assert checkio("2+3") == True, "No brackets, no problem"
+    assert checkio("({[3]})-[4/(3*{1001-1000}*3)/4]") == True, "MUST BE TRUE"
