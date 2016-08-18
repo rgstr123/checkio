39d942163c71437b8e8cbe2dac83df08a821d3ea
rgstr123 <rgstr@inbox.lv>
2016-08-18 04:51:52 +0300

    Расстояние Хэмминга между двумя двоичными числами

---
+++ Electronic-Station/The Hamming Distance.py
@@ -0,0 +1,19 @@
+def checkio(n, m):
+
+    trailing_len = max(len(bin(m))-2, len(bin(n))-2)
+    n = format(n, '0{0}b'.format(trailing_len))
+    m = format(m, '0{0}b'.format(trailing_len))
+    return sum(i!=j for i ,j in zip(n,m))
+
+#############################################################################
+#                             Можно и так
+#
+# def checkio(n, m):
+#     return str(bin(n ^ m)).count('1')
+#############################################################################
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(999999, 1) == 11, "Third example"
